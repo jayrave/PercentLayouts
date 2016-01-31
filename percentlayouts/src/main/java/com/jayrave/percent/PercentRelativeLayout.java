55eb76708b6bd1cb6718c727108a7a7f8bfe0c2d
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2016-01-30 22:01:29 -0800

    add PercentRelativeLayout

---
+++ percentlayouts/src/main/java/com/jayrave/percent/PercentRelativeLayout.java
@@ -0,0 +1,55 @@
+package com.jayrave.percent;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class PercentRelativeLayout extends android.support.percent.PercentRelativeLayout {
+
+    public PercentRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public PercentRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PercentRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(@NonNull AttributeSet attrs) {
+        LayoutParams lp = new LayoutParams(getContext(), attrs);
+        PercentLayoutHelper.checkLayoutParamsFromXml(lp);
+        return lp;
+    }
+
+
+    public static class LayoutParams extends
+            android.support.percent.PercentRelativeLayout.LayoutParams {
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        @Override
+        protected void setBaseAttributes(TypedArray a, int widthAttr, int heightAttr) {
+            PercentLayoutHelper.fetchWidthAndHeight(this, a, widthAttr, heightAttr);
+        }
+    }
+}
