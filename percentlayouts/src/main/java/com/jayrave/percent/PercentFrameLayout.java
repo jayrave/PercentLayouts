ee4b06d1270019bbb499a49c882c5d91b562631d
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2016-01-30 22:01:18 -0800

    add PercentFrameLayout

---
+++ percentlayouts/src/main/java/com/jayrave/percent/PercentFrameLayout.java
@@ -0,0 +1,68 @@
+package com.jayrave.percent;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+public class PercentFrameLayout extends android.support.percent.PercentFrameLayout {
+
+    public PercentFrameLayout(Context context) {
+        super(context);
+    }
+
+    public PercentFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PercentFrameLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(@NonNull AttributeSet attrs) {
+        LayoutParams lp = new LayoutParams(getContext(), attrs);
+        PercentLayoutHelper.checkLayoutParamsFromXml(lp);
+        return lp;
+    }
+
+
+    public static class LayoutParams extends
+            android.support.percent.PercentFrameLayout.LayoutParams {
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(FrameLayout.LayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(android.support.percent.PercentFrameLayout.LayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(int width, int height, int gravity) {
+            super(width, height, gravity);
+        }
+
+        @Override
+        protected void setBaseAttributes(TypedArray a, int widthAttr, int heightAttr) {
+            PercentLayoutHelper.fetchWidthAndHeight(this, a, widthAttr, heightAttr);
+        }
+    }
+}
